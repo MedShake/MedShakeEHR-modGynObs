f6f989b1dc7979a6b80c4a045be68b4f2a9f16cb
Bertrand Boutillier <b.boutillier@gmail.com>
2018-10-17 21:14:24 +0200

    modifications : gestion autocomplete etendu par name et multiple documents pour signature numérique

---
+++ upgrade/gynobs/sqlUpgrade_v3.3.1_v3.4.0.sql
@@ -0,0 +1,12 @@
+-- Modifications des données de la bdd de v3.3.1 à v3.4.0
+
+-- mise à jour pour un autocomplete correct formulaire fin de grossesse
+UPDATE `forms` SET `yamlStructure` = replace(yamlStructure, '3:22:230:235:241', 'firstname:othersfirstname:igPrenomFA:igPrenomFB:igPrenomFC');
+UPDATE `forms` SET `yamlStructureDefaut` = replace(yamlStructureDefaut, '3:22:230:235:241', 'firstname:othersfirstname:igPrenomFA:igPrenomFB:igPrenomFC');
+
+-- upgrade n° de version
+UPDATE `system` SET `value`='v3.4.0' WHERE `name`='gynobs';
+
+-- déplacement du consentement écho à signer
+SET @catID = (SELECT data_cat.id FROM data_cat WHERE data_cat.name='catModelesDocASigner');
+update data_types set cat = @catID where name = 'consentementEcho' limit 1;
