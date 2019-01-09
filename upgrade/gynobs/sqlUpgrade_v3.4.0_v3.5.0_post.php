a28d581f2b61fa72b8b1fafd63c36a9e8269409c
Bertrand Boutillier <b.boutillier@gmail.com>
2019-01-09 18:29:26 +0100

    feat: inclusion du JS formulaire en base

---
+++ upgrade/gynobs/sqlUpgrade_v3.4.0_v3.5.0_post.php
@@ -0,0 +1,10 @@
+<?php
+
+// retrait des anciens fichiers JS formulaire
+@unlink($p['config']['webDirectory'].'/js/module/formsScripts/gynObsEcho12.js');
+@unlink($p['config']['webDirectory'].'/js/module/formsScripts/gynObsEcho22.js');
+@unlink($p['config']['webDirectory'].'/js/module/formsScripts/gynObsEcho32.js');
+@unlink($p['config']['webDirectory'].'/js/module/formsScripts/gynObsEchoAvt11.js');
+@unlink($p['config']['webDirectory'].'/js/module/formsScripts/gynObsEchoGyneco.js');
+@unlink($p['config']['webDirectory'].'/js/module/formsScripts/gynObsFinGrossesse.js');
+@unlink($p['config']['webDirectory'].'/js/module/formsScripts/gynObsMarqueursSeriques.js');
