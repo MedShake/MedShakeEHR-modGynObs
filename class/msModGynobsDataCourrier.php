1b94a892533cf78831a03a96601101463c8460fc
Bertrand Boutillier <b.boutillier@gmail.com>
2019-07-17 11:14:30 +0200

    feat: formulaire DPNI sur base de template PDF

--- class/msModGynobsDataCourrier.php
+++ class/msModGynobsDataCourrier.php
@@ -142,6 +142,29 @@ class msModGynobsDataCourrier
         }
       }
 
+
+/**
+ * Calcule tout le nécessaire à l'établissement du form gynObsDPNI
+ * @param  array $d array des types
+ * @return void
+ */
+      public static function getCrDataCompleteModuleForm_gynObsDPNI(&$d)
+      {
+
+        $name2typeID = new msData();
+        $name2typeID = $name2typeID->getTypeIDsFromName(['echo12']);
+
+        // echo 12 id + date
+        if($e12=msSQL::sqlUnique("select id, creationDate from objets_data where toID='".$d['patientID']."' and instance='".$d['instance']."' and typeID='".$name2typeID['echo12']."' and deleted='' and outdated='' order by id desc limit 1 ")) {
+          $d['dateEcho12'] = $e12['creationDate'];
+          // data echo 12 de la même grossesse
+          $dataE12 = new msCourrier();
+          $d=$d+$dataE12->getExamenData($d['patientID'], 'gynObsEcho12', $e12['id']);
+        }
+
+      }
+
+
 /**
  * Calcule tout le nécessaire à l'établissement du form gynobsGenotypageRhesusFoetalSangMaternel
  * @param  array $d array des types
